644c92548d368c78f97cfe38b501047aaf196597
sleepylinde <k.and.dreyer@googlemail.com>
2022-07-09 17:05:39 +0200

    restructure of course pages with improved code

---
+++ src/coursepages/softwaremanagement/softwaremanagement_prof.php
@@ -0,0 +1,87 @@
+<?php
+if(isset($_POST['submit'])){
+    $desc = $_POST['desc']."";
+
+
+$file=fopen("softwaremanagement_desc.txt", "w");
+fwrite($file, $desc);
+fclose($file);
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Fiktiver Softwaremanagement Kurs an der RFH</title>
+    <link rel="stylesheet" href="../../css/style.css" type="text/css" />
+    <link rel="stylesheet" href="../../css/coursestyle.css" type="text/css" />
+</head>
+<body>
+    <header>
+        <h1>Willkommen zu Ihrem Softwaremanagement Kursangebot! </h1>
+    </header>
+    <main>
+        <div class="scroll text">
+            <p align="left"><font size="4" color="#FFFFFF"><marquee style="border-left: 10; border-right: 10 dashed; border-top: 10 solid; 
+            border-bottom: 10 double" behavior="alternate" scrollamount="20" scrolldelay="30" border="0">
+            Hier können Sie ihre Infos bearbeiten!</marquee></font></p>
+        </div>
+       
+            <div class="course", style="float:left;">
+                <h3> Kursbeschreibung</h3>
+                <h4> Ändern Sie hier ihre Kursbeschreibung!</h4>
+                <form class="desc-text" method="post">
+                <textarea name="desc" rows="40" cols="40"><?php echo trim(file_get_contents('softwaremanagement_desc.txt'));?></textarea>
+                <input type="submit" name="submit" value="Speichern" class="course">
+                
+            </div>
+            <div class="ads", style="float:left;">
+                <br>
+                <p align="center"><img style="height: 90%; width: 90%; object-fit: contain'" src="../../resources/images/werbungstehen.png"></p>
+            </div>
+            <div class="content", style="float:right;">
+                <h3>Hier können Sie Ihre Files hochladen:</h3>
+                <a href="/fileUpload.html">File Upload</a>
+                    
+                    <br>
+
+                    <h3>Übersicht ihrer Files</h3>
+                    <p><?php
+                    $dir_path = "../../resources/course files/softwaremanagement/"; 
+                    $ignoreList = array('.', '..', 'abgaben');
+
+                        if (is_dir($dir_path)) {
+                            if ($dir_handler = opendir($dir_path)) {
+                                while (($file = readdir($dir_handler)) !== false) {
+                                    if(!in_array($file, $ignoreList)){
+                                        echo "<a href='{$dir_path}{$file}' download = '$file'> $file</a><br>";
+                                    }
+                                 }
+                            closedir($dir_handler);
+                            }
+                        }
+                    ?>
+            </div>
+            <div class="upload">
+                <p> Hier sehen Sie die wöchentlichen Hausaufgaben der Studenten:</p>
+                <p><?php
+                    $dir_path = "../../resources/course files/softwaremanagement/abgaben/"; 
+                    $ignoreList = array('.', '..');
+
+                        if (is_dir($dir_path)) {
+                            if ($dir_handler = opendir($dir_path)) {
+                                while (($file = readdir($dir_handler)) !== false) {
+                                    if(!in_array($file, $ignoreList)){
+                                        echo "<a href='{$dir_path}{$file}' download = '$file'> $file</a><br>";
+                                    }
+                                 }
+                            closedir($dir_handler);
+                            }
+                        }
+                        ?>
+                
+            </div>
+
+    </main>
+    
+</body>
---
+++ src/coursepages/softwaremanagement/softwaremanagement_student.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Fiktiver Softwaremanagement Kurs an der RFH</title>
+    <link rel="stylesheet" href="../../css/style.css" type="text/css" />
+    <link rel="stylesheet" href="../../css/coursestyle.css" type="text/css" />
+</head>
+<body>
+    <header>
+        <h1>Willkommen zu unserem Softwaremanagement Kursangebot! </h1>
+    </header>
+    <main>
+        <div class="scroll text">
+            <p align="left"><font size="4" color="#FFFFFF"><marquee style="border-left: 10; border-right: 10 dashed; border-top: 10 solid; 
+            border-bottom: 10 double" behavior="alternate" scrollamount="20" scrolldelay="30" border="0">
+            Hier gibt es alle wichtigen Infos!!</marquee></font></p>
+        </div>
+       
+            <div class="course", style="float:left;">
+                <h3> Kursbeschreibung</h3>
+                <p><?php include('softwaremanagement_desc.txt'); ?></p>
+            </div>
+            <div class="ads", style="float:left;">
+                <br>
+                <p align="center"><img src="../../resources/images/98plusani.gif" alt="98plusani.gif (8405 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/0.gif" alt="0.gif (7891 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/1001ffbutton.gif" alt="1001ffbutton.gif (4151 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/1001freenewfontsbutton.gif" alt="1001freenewfontsbutton.gif (3772 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/addchannel.gif" alt="addchannel.gif (1768 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/discord.gif" alt="discord.gif (10038 bytes)" WIDTH="88" HEIGHT="31"><br>
+                    <img src="../../resources/images/button-7.gif" alt="button-7.gif (1854 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/classic_compatible.gif" alt="classic_compatible.gif (1055 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/debian-powered.gif" alt="debian-powered.gif (2596 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/DesignedByMacintosh.gif" alt="DesignedByMacintosh.gif (19960 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/geocities_mirr.gif" alt="geocities_mirr.gif (1477 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/ie_anim.gif" alt="ie_anim.gif (5432 bytes)" WIDTH="88" HEIGHT="31"><br>
+                    <img src="../../resources/images/notepad2.gif" alt="notepad2.gif (1154 bytes)" WIDTH="88" HEIGHT="30"><img src="../../resources/images/discord2.gif" alt="discord2.gif (1210 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/click_here.gif" alt="click_here.gif (4079 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/clickhere_blue.gif" alt="clickhere_blue.gif (2783 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/clickhere_red.gif" alt="clickhere_red.gif (2783 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/netscape2.gif" alt="netscape2.gif (9401 bytes)" WIDTH="88" HEIGHT="31"><br>
+                    <img src="../../resources/images/comdex4.gif" alt="comdex4.gif (840 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/getwmp7.gif" alt="getwmp7.gif (13943 bytes)" WIDTH="89" HEIGHT="32"><img src="../../resources/images/dell_88x31.gif" alt="dell_88x31.gif (1928 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/hotmail3.gif" alt="hotmail3.gif (1587 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/ie_anim.gif" alt="ie_anim.gif (5432 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/macos_mov.gif" alt="macos_mov.gif (6956 bytes)" WIDTH="88" HEIGHT="31"><br>
+                    <img src="../../resources/images/internet_now.gif" alt="internet_now.gif (8881 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/macintoshwks.gif" alt="macintoshwks.gif (7350 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/macmade-blkwtbrdrspin.gif" alt="macmade-blkwtbrdrspin.gif (11372 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/macos_mov.gif" alt="macos_mov.gif (6956 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/getwmp7.gif" alt="getwmp7.gif (13943 bytes)" WIDTH="89" HEIGHT="32"><img src="../../resources/images/macintoshwks.gif" alt="macintoshwks.gif (7350 bytes)" WIDTH="88" HEIGHT="31"><br>
+                    <img src="../../resources/images/got_html.gif" alt="got_html.gif (1142 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/macos8.gif" alt="macos8.gif (2156 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/macosmade.gif" alt="macosmade.gif (7545 bytes)" WIDTH="80" HEIGHT="32"><img src="../../resources/images/madewithamiga2.gif" alt="madewithamiga2.gif (3740 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/microsoft3.gif" alt="microsoft3.gif (14942 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/netscape.gif" alt="netscape.gif (40190 bytes)" WIDTH="88" HEIGHT="31"><br>
+                    <img src="../../resources/images/microsoftlogo.gif" alt="microsoftlogo.gif (14871 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/netscape.gif" alt="netscape.gif (40190 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/norton.gif" alt="norton.gif (8192 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/members-choice.gif" alt="members-choice.gif (921 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/office97.gif" alt="office97.gif (6077 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/internet_now.gif" alt="internet_now.gif (8881 bytes)" WIDTH="88" HEIGHT="31"><br>
+                    <img src="../../resources/images/darwin.gif" alt="darwin.gif (970 bytes)" WIDTH="89" HEIGHT="31"><img src="../../resources/images/netscape.gif" alt="netscape.gif (40190 bytes)" WIDTH="88" HEIGHT="31"><img src="../../resources/images/microsoft.gif" alt="microsoft.gif (14687 bytes)" WIDTH="100" HEIGHT="31"><img src="../../resources/images/addchannel.gif" alt="addchannel.gif (1768 bytes)" WIDTH="88" HEIGHT="31"></p></p>
+                <br>
+                <p align="center"></p>
+            </div>
+            <div class="content" style="float:right;">
+                <h3> Download Bereich</h3>
+                <p><?php
+                    $dir_path = "../../resources/course files/softwaremanagement/"; 
+                    $ignoreList = array('.', '..', 'abgaben');
+
+                        if (is_dir($dir_path)) {
+                            if ($dir_handler = opendir($dir_path)) {
+                                while (($file = readdir($dir_handler)) !== false) {
+                                    if(!in_array($file, $ignoreList)){
+                                        echo "<a href='{$dir_path}{$file}' download = '$file'> $file</a><br>";
+                                    }
+                                 }
+                            closedir($dir_handler);
+                            }
+                        }
+                        ?>
+            </div>
+            <div class="upload">
+                <h3>Hier können Sie wöchentlichen Hausaufgaben hochladen:</h3>
+                <a href="/fileUpload.html">File Upload</a>
+                
+            </div>
+
+    </main>
+    
+</body>
